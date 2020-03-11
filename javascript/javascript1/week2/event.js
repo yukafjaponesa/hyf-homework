561520d20719742caf1ae39b74c6594b9f466230
yukafjaponesa <yfukuzawa318@gmail.com>
2020-03-11 20:31:18 +0100

    deleted unnecessary files

--- javascript/javascript1/week2/event.js
+++
@@ -1,8 +0,0 @@
-let currentDate = new Date();
-function getEventWeekday(days) {
-  return new Date(currentDate.getTime() + days * 24 * 60 * 1000)
-};
-
-let days = prompt("please enter event days up to now");
-let newDate = getEventWeekday(days);
-console.log("The even will be held on this date:" + newDate);
