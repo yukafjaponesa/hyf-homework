e75591f9721ddeb8cfa59980c550e2112163d44f
yukafjaponesa <yfukuzawa318@gmail.com>
2020-03-29 23:09:44 +0200

    made changes

--- javascript/javascript2/week1/main.js
+++ javascript/javascript2/week1/main.js
@@ -10,17 +10,26 @@ function renderProducts(products) {
   for(let i = 0; i < products.length; i++) {
   const li = document.createElement('li');
   const ulInside = document.createElement('ul');
-  ul.appendChild(li);
-  li.appendChild(ulInside);
+  li.innerHTML = products[i].name;
+
+  for(let j = 0; j < products.length; j++){
   const liInside = document.createElement('li');
-  liInside.innerHTML = products[i].name;
+  liInside.innerHTML = products[j].name;
+  liInside.innerHTML += ' | ' + products[j].id
+  liInside.innerHTML += ' | ' + products[j].price
+  liInside.innerHTML += ' | ' + products[j].rating
+  liInside.innerHTML += ' | ' + products[j].shipsTo
   ulInside.appendChild(liInside);
-    //li.innerHTML = products[i].name;
-    //li.innerHTML += ' | ' + products[i].id
-    //li.innerHTML += ' | ' + products[i].price
-    //li.innerHTML += ' | ' + products[i].rating
-    //li.innerHTML += ' | ' + products[i].shipsTo
-    ul.appendChild(li);
+  }
+
+  li.appendChild(ulInside);
+  ul.appendChild(li);
+
+/*    li.innerHTML = products[i].name;
+    li.innerHTML += ' | ' + products[i].id
+    li.innerHTML += ' | ' + products[i].price
+    li.innerHTML += ' | ' + products[i].rating
+    li.innerHTML += ' | ' + products[i].shipsTo*/
 }
 };
 let a = renderProducts(testProductNames); //testProductNames becomes products
