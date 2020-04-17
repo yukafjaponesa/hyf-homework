3581c6fe9d78e98daa176b49f6e48a90042f31fc
yukafjaponesa <yfukuzawa318@gmail.com>
2020-04-17 17:26:26 +0200

    added homework for js2 week3

---
+++ javascript/javascript2/week3/jokes.js
@@ -0,0 +1,15 @@
+const jokes = [
+  "Why is Peter Pan always flying? He neverlands.",
+  "Why didn't the astronaut come home to his wife? He needed his space.",
+  "I wasn't going to visit my family this December, but my mom promised to make me Eggs Benedict. So I'm going home for the hollandaise.",
+  "What did the blanket say as it fell off the bed? Oh sheet!"
+];
+
+const badJoke= [
+  "Did you hear about the Italian chef who died? He pasta-way.",
+  "I sold my vacuum the other day.All it was doing was collecting dust.",
+  "I have many jokes about unemployed people. Sadly none of them work.",
+  "What’s the best thing about Switzerland? Well the flag’s a big plus."
+];
+
+module.exports = {jokes, badJoke}
