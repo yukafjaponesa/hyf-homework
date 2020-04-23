b086f29dfc6d9af56d1a10b7458b981c867553da
yukafjaponesa <yfukuzawa318@gmail.com>
2020-04-23 15:58:45 +0200

    implemented feed back

--- javascript/javascript1/week1/Ez-Namey.js
+++ javascript/javascript1/week1/Ez-Namey.js
@@ -1,9 +1,12 @@
 const firstWords = ["Easy", "Awesome", "Corporate", "Bright", "Advanced"];
 const secondWords = ["Life", "Action", "Together", "Way", "Future"];
 
-const startupName = ;
-
-function startupName () {
-  Math.floor(Math.random())
-  console.log(startupName(firstWords, secondWords))
+function getStartUpName (firstWords, secondWords) {
+  const firstName = firstWords[Math.floor(Math.random() * firstWords.length)];
+  const secondName = secondWords[Math.floor(Math.random() * secondWords.length)];
+  return firstName + " " + secondName;
 };
+
+const startupName = getStartUpName(["Easy", "Awesome", "Corporate", "Bright", "Advanced"], ["Life", "Action", "Together", "Way", "Future"]);
+
+console.log(startupName);
