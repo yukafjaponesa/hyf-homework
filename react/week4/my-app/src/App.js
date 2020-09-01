81435af3bc16176a17efdbf01bbe884a957cb02f
Yuka Fukuzawa <yfukuzawa318@gmail.com>
2020-09-01 12:23:20 +0200

    changed

--- react/week4/my-app/src/App.js
+++ react/week4/my-app/src/App.js
@@ -5,8 +5,41 @@ import {
   Route,
   Link
 } from "react-router-dom";
-import {Navbar} from "./src-backend/Navbar";
+import {About} from "./components/About";
+import {Home} from "./components/Home";
+import {Users} from "./components/Users";
 
 export default function App() {
+  return(
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
 
-}
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/users">
+            <Users />
+          </Route>
+          <Route path="/">
+            <Home />
+            <Nav />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+};
